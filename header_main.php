c75eba925b8a70b47e14c6606649e1617cce81ff
ddjyogi <yogendra.tanwar@gmail.com>
2016-10-14 19:28:23 -0400

    image and header modification

--- header_main.php
+++ header_main.php
@@ -14,12 +14,12 @@
         </div>
         <div class="header_mbar">            
             <div class="hmenu">                
-                <div><a href="index.php?index"><li class="header_mblock">Home</li></a></div>
-                <div><a href="index.php?about_us"><li class="header_mblock">About Us</li></a></div>
-                <div><a href="index.php?portfolio"><li class="header_mblock">Portfolio</li></a></div>
+                <div><a href="index.php?index"><li class="header_mblock hhome">Home</li></a></div>
+                <div><a href="index.php?about_us"><li class="header_mblock habout_us">About Us</li></a></div>
+                <div><a href="index.php?portfolio"><li class="header_mblock hportf">Portfolio</li></a></div>
                 <div><a href="index.php?services"><li class="header_mblock">Services</li></a></div>
-                <div><a href="index.php?blog"><li class="header_mblock">Blog</li></a></div>
-                <div><a href="index.php?contact_us"><li class="header_mblock">Contact Us</li></a></div>
+                <div><a href="index.php?blog"><li class="header_mblock hblog">Blog</li></a></div>
+                <!--<div><a href="index.php?contact_us"><li class="header_mblock">Contact Us</li></a></div>-->
             </div>
         </div>        
     </div>    
