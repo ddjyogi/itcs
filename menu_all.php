a4e104ab09cfd8e510e30b46d9d1e468e3afef25
ddjyogi <yogendra.tanwar@gmail.com>
2016-11-12 13:02:26 -0500

    --all

---
+++ menu_all.php
@@ -0,0 +1,9 @@
+<div class="header_mbar">            
+    <div class="hmenu">                
+    <div><a href="index.php?index"><li class="header_mblock hhome">Home</li></a></div>
+    <div><a href="index.php?about_us"><li class="header_mblock habout_us">About Us</li></a></div>
+    <div><a href="index.php?portfolio"><li class="header_mblock hportf">Portfolio</li></a></div>
+    <div><a href="index.php?services"><li class="header_mblock hservice hserimg">Services</li></a></div>
+    <div><a href="index.php?blog"><li class="header_mblock hblog">Blog</li></a></div>
+    </div>
+</div>
