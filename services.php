5de79903e2848f82a9cf7c1c158a7e1cae84d872
ddjyogi <yogendra.tanwar@gmail.com>
2016-11-13 19:38:12 -0500

    --all

--- services.php
+++ services.php
@@ -6,14 +6,16 @@
         <div class="serblock1">
             
         </div>
-        <div class="serblock2">
-            <div class="serblock2img1"><a href="index.php?2451541r654se"><img src="../itcs/images/services1.png" width="100%" height="100%" /></a></div>
-            <div class="serblock2img1"><a href="index.php?2451541op654s"><img src="../itcs/images/services2.png" width="100%" height="100%" /></a></div>
-            <div class="serblock2img1"><a href="index.php?2451541c654sc"><img src="../itcs/images/services3.png" width="100%" height="100%" /></a></div>
-            <div class="serblock2img1"><a href="index.php?2451541b654se"><img src="../itcs/images/services4.png" width="100%" height="100%" /></a></div>
-            <div class="serblock2img1"><a href="index.php?2451541a654mr"><img src="../itcs/images/services5.png" width="100%" height="100%" /></a></div>
-            <div class="serblock2img1"><a href="index.php?2451541r654td"><img src="../itcs/images/services6.png" width="100%" height="100%" /></a></div>
-        </div>
+        <!--<div class="serblock2">
+            <div class="serblock2img1"><a href="index.php?2451541r654se"><img src="../itcs/images/services1.png" width="63%" height="100%" /></a></div>
+            <div class="serblock2img1"><a href="index.php?2451541c654sc"><img src="../itcs/images/services3.png" width="63%" height="100%" /></a></div>
+            <div class="serblock2img1"><a href="index.php?2451541op654s"><img src="../itcs/images/services2.png" width="63%" height="100%" /></a></div>
+        </div>    
+        <div class="serblock2">    
+            <div class="serblock2img1"><a href="index.php?2451541a654mr"><img src="../itcs/images/services5.png" width="63%" height="100%" /></a></div>
+            <div class="serblock2img1"><a href="index.php?2451541b654se"><img src="../itcs/images/services4.png" width="63%" height="100%" /></a></div>
+            <div class="serblock2img1"><a href="index.php?2451541r654td"><img src="../itcs/images/services6.png" width="63%" height="100%" /></a></div>
+        </div>-->
         <div class="serblock3">
             
         </div>
